6b8c3e3e010891fc33a06edfc588dc1ebc30bb39
dorcky91 <orlandoappolon1@gmail.com>
2023-08-18 21:55:28 -0600

    Nueva Sección

--- src/CARPETA_COMPONENTES/Hero/Hero.jsx
+++ src/CARPETA_COMPONENTES/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import "../Hero/hero.css";
+import "../Hero/Hero.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHeart,
